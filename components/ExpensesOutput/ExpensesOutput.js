044dd2bbba4d30cca0e7882cbf2cb0c281b054b6
GABRIEL AKAHOSHI COLLADO (CRGL-THIRDPARTY.COM) <GABRIEL_AKAHOSHICOLLADO@CRGL-THIRDPARTY.COM>
2024-08-15 13:46:57 -0300

    Implement context state management

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,40 +1,18 @@
-import { View, StyleSheet } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 import { GlobalStyles } from "../../constants/styles";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    description: "A pair of shoes",
-    amount: 59.99,
-    date: new Date("2024-07-19"),
-  },
-  {
-    id: "e2",
-    description: "A pair of trousers",
-    amount: 89.99,
-    date: new Date("2024-07-01"),
-  },
-  {
-    id: "e3",
-    description: "Diamond ring",
-    amount: 450.0,
-    date: new Date("2024-08-12"),
-  },
-  {
-    id: "e4",
-    description: "Fishing equipment",
-    amount: 150.0,
-    date: new Date("2024-08-10"),
-  },
-];
+const ExpensesOutput = ({ expenses, expensesPeriod, fallbackText }) => {
+  let content = <Text style={styles.infoText}>{fallbackText}</Text>
 
-const ExpensesOutput = ({ expenses, expensesPeriod }) => {
+  if (expenses.length > 0) {
+    content = <ExpensesList expenses={expenses} />
+  }
   return (
     <View style={styles.container}>
-      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
-      <ExpensesList expenses={DUMMY_EXPENSES} />
+      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      {content}
     </View>
   );
 };
@@ -48,5 +26,11 @@ const styles = StyleSheet.create({
     paddingTop: 24,
     paddingHorizontal: 24,
     backgroundColor: GlobalStyles.colors.primary700,
+  },
+  infoText: {
+    color: "white",
+    fontSize: 16,
+    textAlign: "center",
+    marginTop: 32,
   },
 });
---
+++ store/expenses-context.js
@@ -0,0 +1,87 @@
+import { createContext, useReducer } from "react";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2024-07-19"),
+  },
+  {
+    id: "e2",
+    description: "A pair of trousers",
+    amount: 89.99,
+    date: new Date("2024-07-01"),
+  },
+  {
+    id: "e3",
+    description: "Diamond ring",
+    amount: 450.0,
+    date: new Date("2024-08-12"),
+  },
+  {
+    id: "e4",
+    description: "Fishing equipment",
+    amount: 150.0,
+    date: new Date("2024-08-10"),
+  },
+];
+
+export const ExpensesContext = createContext({
+  expenses: [],
+  addExpense: ({ description, amount, date }) => {},
+  deleteExpense: (id) => {},
+  updateExpense: (id, { description, amount, date }) => {},
+});
+
+const expensesReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      const id = new Date().toString() + Math.random().toString();
+      return [{ ...action.payload, id }, ...state];
+    case "UPDATE":
+      const updatableExpenseIndex = state.findIndex(
+        (expense) => expense.id === action.payload.id
+      );
+      const updatableExpense = state[updatableExpenseIndex];
+      const updatedItem = { ...updatableExpense, ...action.payload.data };
+      const updatedExpenses = [...state];
+      updatedExpenses[updatableExpenseIndex] = updatedItem;
+      return updatedExpenses;
+    case "DELETE":
+      return state.filter((expense) => expense.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+const ExpensesContextProvider = ({ children }) => {
+  const [expensesState, dispatch] = useReducer(expensesReducer, DUMMY_EXPENSES);
+
+  const addExpense = (expenseData) => {
+    dispatch({ type: "ADD", payload: expenseData });
+  };
+
+  const deleteExpense = (id) => {
+    dispatch({ type: "DELETE", payload: id });
+  };
+
+  const updateExpense = (id, expenseData) => {
+    dispatch({ type: "UPDATE", payload: { id, data: expenseData } });
+  };
+
+  const value = {
+    expenses: expensesState,
+    addExpense,
+    deleteExpense,
+    updateExpense,
+  };
+
+  return (
+    <ExpensesContext.Provider value={value}>
+      {children}
+    </ExpensesContext.Provider>
+  );
+};
+
+export default ExpensesContextProvider;
